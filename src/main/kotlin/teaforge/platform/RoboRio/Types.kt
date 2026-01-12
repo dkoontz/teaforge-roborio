813028bbb0010ac1f1588ce98b7dbfdebc69440e
maxbrennan <maxwbrennan@gmail.com>
2026-01-12 16:34:50 -0700

    web-socket (#8)
    * Revert "Removed required CanDeviceType in Effect.InitCanDevice (required complex refactor)"
    
    This reverts commit efc30b69c33390ccab1b5f16ebbca75b79432dce.
    
    * Undo CandDevice refactor
    
    * Delete print 10 bug and update to 2026

--- src/main/kotlin/teaforge/platform/RoboRio/Types.kt
+++ src/main/kotlin/teaforge/platform/RoboRio/Types.kt
@@ -178,6 +178,13 @@ sealed interface CanDeviceToken {
     ) : CanDeviceToken
 }
 
+enum class CanDeviceType {
+    Neo,
+    Talon,
+    Encoder,
+    Pigeon,
+}
+
 sealed interface GamepadButtonState {
     object Pressed : GamepadButtonState
 
